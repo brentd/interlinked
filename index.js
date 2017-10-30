2cc66a8f99103456de64902f2c1330e976eb00c1
Brent Dillingham <brentdillingham@gmail.com>
2017-10-29 20:41:45 -0400

    Organize stream setup and mux/demux into Channel class

--- index.js
+++ index.js
@@ -4,28 +4,22 @@ rx.Observable.prototype.log = function(msg) {
   return this.do(x => console.log(msg, x))
 }
 
-const demux = (muxedStream, key) => {
-  return muxedStream.filter(x => x[0] === key).map(x => x[1])
-}
-
-const mux = key => x => [key, x]
-
 let subscriptionId = 0
 
-const remoteToLocal = (input, mainOut, registers, completes) => remote => {
+const remoteToLocal = channel => remote => {
   return Object.entries(remote).reduce((local, [k, v]) => {
     if (v.type === 'observable') {
       const proxy = rx.Observable.create(observer => {
         const id = subscriptionId++
-        const complete = completes.filter(x => x === id)
-        const sub = demux(input, id).takeUntil(complete).subscribe(observer)
-        mainOut.next({subscribe: k, id})
+        const complete = channel.completes.filter(x => x === id)
+        const sub = channel.demux(id).takeUntil(complete).subscribe(observer)
+        channel.main.send({subscribe: k, id})
         return () => {
           sub.unsubscribe()
-          mainOut.next({unsubscribe: id})
+          channel.main.send({unsubscribe: id})
         }
       })
-      local[k] = proxy.takeUntil(registers)
+      local[k] = proxy.takeUntil(channel.registers)
     } else if (v.type === 'function') {
       local[k] = () => console.log('calling function', k)
     } else {
@@ -48,47 +42,57 @@ const localToRemote = local => {
   }, {})
 }
 
-export default function(input, output, api) {
-  const main = {
-    in: demux(input, '@').share(),
-    out: new rx.Subject()
+// Represents the multiplexed pipe for a peer. Exists only to organize - it does
+// not subscribe to any streams itself.
+class Channel {
+  constructor(input, sender) {
+    this.input = input
+    this.send  = (id, x) => sender([id, x])
+
+    this.main = {
+      in: this.demux('@').share(),
+      send: x => this.send('@', x)
+    }
+
+    this.registers = this.main.in
+      .filter(x => x.register)
+      .pluck('register')
+
+    this.subscribes = this.main.in
+      .filter(x => x.subscribe != undefined)
+      .map(({subscribe, id}) => [subscribe, id])
+
+    this.unsubscribes = this.main.in
+      .filter(x => x.unsubscribe != undefined)
+      .pluck('unsubscribe')
+
+    this.completes = this.main.in
+      .filter(x => x.complete != undefined)
+      .pluck('complete')
   }
-  main.out.map(mux('@')).subscribe(output)
+
+  demux(id) {
+    return this.input.filter(x => x[0] === id).map(x => x[1])
+  }
+}
+
+export default function(input, output, api) {
+  const channel = new Channel(input, x => output.next(x))
 
   if (api && Object.keys(api).length > 0)
-    main.out.next({register: localToRemote(api)})
+    channel.main.send({register: localToRemote(api)})
 
-  const registers = main.in
-    .filter(x => x.register)
-    .pluck('register')
-
-  const subscribes = main.in
-    .filter(x => x.subscribe)
-    .map(({subscribe, id}) => [subscribe, id])
-
-  const unsubscribes = main.in
-    .filter(x => x.unsubscribe != undefined)
-    .pluck('unsubscribe')
-
-  const completes = main.in
-    .filter(x => x.complete)
-    .pluck('complete')
-
-  // const methods = main
-  //   .filter(x => x.method)
-  //   .map(({method, id}))
-
-  subscribes
+  channel.subscribes
     .map(([name, id]) =>
       api[name]
-        .takeUntil(unsubscribes.filter(x => x === id))
+        .takeUntil(channel.unsubscribes.filter(x => x === id))
         .subscribe(
-          x   => output.next(mux(id)(x)),
-          err => main.out.next({error: id}),
-          ()  => main.out.next({complete: id})
+          x   => channel.send(id, x),
+          err => channel.main.send({error: id}),
+          ()  => channel.main.send({complete: id})
         )
     )
     .subscribe()
 
-  return registers.map(remoteToLocal(input, main.out, registers, completes))
+  return channel.registers.map(remoteToLocal(channel))
 }
